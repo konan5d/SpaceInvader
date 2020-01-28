affbc3b7d4a01965735d36c8c40e86e8a1b24257
Corentin LEGEAY <corentin.legeay@ynov.com>
2020-01-28 09:34:56 +0100

    Include headers files vt100.h and serial.h

---
+++ spaceinvader/src/main.h
@@ -0,0 +1,16 @@
+/*
+ * main.h
+ *
+ *  Created on: 28 janv. 2020
+ *      Author: korantin
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include "vt100.h"
+#include "serial.h"
+
+
+
+#endif /* MAIN_H_ */
