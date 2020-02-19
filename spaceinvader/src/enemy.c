dfd2d1ca596b46f343abcb898a0c3c0ac2856921
Corentin LEGEAY <corentin.legeay@ynov.com>
2020-02-19 17:13:16 +0100

    Create header file 'Enemy'

---
+++ spaceinvader/src/enemy.c
@@ -0,0 +1,12 @@
+/*
+ * enemy.c
+ *
+ *  Created on: 19 févr. 2020
+ *      Author: korantin
+ */
+
+#include "enemy.h"
+
+static void moveOneEnemyX(t_ship enemy){
+	;
+}
---
+++ spaceinvader/src/enemy.h
@@ -0,0 +1,17 @@
+/*
+ * enemy.h
+ *
+ *  Created on: 19 févr. 2020
+ *      Author: korantin
+ */
+
+#ifndef ENEMY_H_
+#define ENEMY_H_
+
+#include "main.h"
+
+static void moveOneEnemyX(t_ship enemy);
+
+void moveAllEnemies(void);
+
+#endif /* ENEMY_H_ */
