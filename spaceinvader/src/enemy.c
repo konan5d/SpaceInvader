3c41cba46ce15e7def2f3509bae41096d9d1d796
Corentin LEGEAY <corentin.legeay@ynov.com>
2020-02-19 18:09:45 +0100

    Create functions : moveOneEnemyX(), moveEnemyY() and moveEnemiesX()

--- spaceinvader/src/enemy.c
+++ spaceinvader/src/enemy.c
@@ -7,6 +7,24 @@
 
 #include "enemy.h"
 
-static void moveOneEnemyX(t_ship enemy){
-	;
+
+/* Permet de bouger 1 vaisseau enemy */
+void moveOneEnemyX(t_ship *enemy, uint8_t direction){
+	if(direction == RIGHT){
+		enemy->pos_x++;
+	}else if (direction == LEFT){
+		enemy->pos_x--;
+	}
 }
+
+void moveOneEnemyY(t_ship *enemy){
+	enemy->pos_y++;;
+}
+
+void moveEnemiesX(t_ship *enemy, uint8_t direction){
+	uint8_t i;
+	for (i = 0; i <= ENEMIES; i++){
+		moveOneEnemyX(&enemy[i], direction);
+	}
+}
+
