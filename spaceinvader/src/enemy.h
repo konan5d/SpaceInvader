bc2dfd98d5b54e7b19d0e671818a59516596c764
Corentin LEGEAY <corentin.legeay@ynov.com>
2020-02-19 18:09:55 +0100

    Create functions : moveOneEnemyX(), moveEnemyY() and moveEnemiesX()

--- spaceinvader/src/enemy.h
+++ spaceinvader/src/enemy.h
@@ -9,9 +9,24 @@
 #define ENEMY_H_
 
 #include "main.h"
+#include "stdint.h"
 
-static void moveOneEnemyX(t_ship enemy);
 
-void moveAllEnemies(void);
+/*typedef uint32_t t_baudrate;
+typedef uint8_t t_character;
+typedef uint8_t t_pos;
+typedef struct {
+	t_character pos_x;
+	t_character pos_y;
+	t_character new_pos_y;
+	t_character ship;
+	uint8_t life;
+} t_ship;*/
+
+void moveOneEnemyX(t_ship *enemy, uint8_t direction);
+
+void moveOneEnemyY(t_ship *enemy);
+
+void moveEnemiesX(t_ship *enemy, uint8_t direction);
 
 #endif /* ENEMY_H_ */
