99ef18afa364b94b7708d4a23cf8a31e1252dee2
Corentin LEGEAY <corentin.legeay@ynov.com>
2020-02-24 12:16:49 +0100

    Add typedef t_player Add function delay() Change number of enemies Add player score

--- spaceinvader/src/main.h
+++ spaceinvader/src/main.h
@@ -11,15 +11,16 @@
 #define PLAYER_POSITION_X (40)
 #define PLAYER_POSITION_Y (22)
 #define PLAYER_SHIP ('M')
+#define PLAYER_SCORE (0)
 #define PLAYER_LIFE (3)
 
-#define ENEMY_POSITION_X (9)
+#define ENEMY_POSITION_X (19)
 #define ENEMY_POSITION_Y (3)
 #define ENEMY_SHIP ('H')
 #define ENEMY_LIFE (1)
 
-#define ENEMIES (165)
-#define ENEMIES_PER_LINE (33)
+#define ENEMIES (115)//165
+#define ENEMIES_PER_LINE (23)
 #define ENEMIES_PER_COL (5)
 
 #define BAUDRATE (115200)
@@ -40,16 +41,30 @@ typedef struct {
 	uint8_t life;
 } t_ship;
 
-enum way {LEFT, RIGHT};
-enum booleen {FALSE, TRUE};
+typedef struct {
+	t_character pos_x;
+	t_character pos_y;
+	t_character ship;
+	uint16_t score;
+	uint8_t life;
+} t_player;
+
+enum way {
+	LEFT, RIGHT
+};
+enum booleen {
+	FALSE, TRUE
+};
 
 void initEnemy(t_ship *tab_enemies, t_ship enemy, uint8_t enemy_in_line,
 		uint8_t nbr_of_line);
 
-void initPlayground(uint8_t tab_playground[80][24],t_ship *tab_enemies);
+void initPlayground(uint8_t tab_playground[80][24], t_ship *tab_enemies);
 
+void displayEnemiesOnPlayground(uint8_t tab_playground[80][24],
+		t_ship *tab_enemies);
 
-void displayEnemiesOnPlayground(uint8_t tab_playground[80][24], t_ship *tab_enemies);
+void delay(uint32_t time);
 
 uint8_t moveShipLR(t_character *tab_player, uint8_t way, t_pos old_pos);
 #endif /* MAIN_H_ */
