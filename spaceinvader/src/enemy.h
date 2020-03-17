319e0b40ec4ca37698c22c8910d4af7da4ef963c
Corentin LEGEAY <corentin.legeay@ynov.com>
2020-03-17 18:39:10 +0100

    enemy.h : delete code

--- spaceinvader/src/enemy.h
+++ spaceinvader/src/enemy.h
@@ -11,18 +11,6 @@
 #include "main.h"
 #include "stdint.h"
 
-
-/*typedef uint32_t t_baudrate;
-typedef uint8_t t_character;
-typedef uint8_t t_pos;
-typedef struct {
-	t_character pos_x;
-	t_character pos_y;
-	t_character new_pos_y;
-	t_character ship;
-	uint8_t life;
-} t_ship;*/
-
 void moveOneEnemyX(t_ship *enemy, uint8_t direction);
 
 void moveOneEnemyY(t_ship *enemy);
